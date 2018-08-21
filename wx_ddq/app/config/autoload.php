b03d5fd8eb3fba525ecb45b2a4925a09691f2779
ColorfullyZhang <colorfully.zhang@aliyun.com>
2018-08-21 17:29:04 +0800

    .

--- wx_ddq/app/config/autoload.php
+++ wx_ddq/app/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('my_uri', 'parser');
+$autoload['libraries'] = array('parser');
 
 /*
 | -------------------------------------------------------------------
