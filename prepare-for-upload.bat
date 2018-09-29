8de92c93026b032d8d74726f00da6f943d7191c7
Colorfully Zhang <colorfully.zhang@aliyun.com>
2018-09-30 07:13:35 +0800

    .

--- prepare-for-upload.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo 将删除所有临时生成的文件
-pause
-DEL /P /F %~dp0\wx_ddq\data\raw\Africa_data.xlsx
-DEL /P /F %~dp0\wx_ddq\data\runtime\querylog.txt
-DEL /P /F %~dp0\wx_ddq\logs\ci\log-2018-??-??.log
-pause
