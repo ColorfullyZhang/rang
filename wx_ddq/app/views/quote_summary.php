6b173c70b29fdc3ba99c62940a0815016e5f1908
ColorfullyZhang <colorfully.zhang@aliyun.com>
2018-10-15 17:58:49 +0800

    .

--- wx_ddq/app/views/quote_summary.php
+++ wx_ddq/app/views/quote_summary.php
@@ -6,16 +6,16 @@ foreach ($items as $item) {
         echo $dest.PHP_EOL;
     }
     $echo = array(
+        $item['shipOwner'],
         date('md', strtotime($item['startDate'])),
         date('md', strtotime($item['endDate'])),
         sprintf('%d', $item['ctn20g']+$item['ebs20g']+$item['amsens']).'/'.
         sprintf('%d', $item['ctn40g']+$item['ebs40g']+$item['amsens']).'/'.
         sprintf('%d', $item['ctn40h']+$item['ebs40h']+$item['amsens']),
-        $item['shipOwner'],
         str_replace('周', '', $item['weekday']),
         str_replace(['港', '山', '一', '二', '三', '四', '五'], '', $item['loadPort']),
         $item['transitPort'] == '直达' ? '直' : '转',
         $item['duration']
     );
-    echo implode('|', $echo).PHP_EOL;
+    echo ((strtotime($item['endDate']) < strtotime('+4 day')) ? '-' : '').implode('|', $echo).PHP_EOL;
 }
