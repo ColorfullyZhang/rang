48d4dae4300cb9de85f9fffa3d2e02f7d5dff21f
张冬生 <colorfully.zhang@aliyun.com>
2019-06-22 09:42:03 +0800

    .

--- wx_ddq/app_yjc/libraries/Exceldata.php
+++ wx_ddq/app_yjc/libraries/Exceldata.php
@@ -41,21 +41,21 @@ class Exceldata {
             if (in_array($content, [mb_substr($activeSheet->getCell('F'.$r)->getValue(), -1),
                                     substr($activeSheet->getCell('G'.$r)->getValue(), -3),
                                     substr($activeSheet->getCell('H'.$r)->getValue(), -3)])) {
-                $projects[] = str_replace(' ', '', $activeSheet->getCell('B'.$r)->getValue()).
-                    date('ymd', PHPExcel_Shared_Date::ExcelToPHP($activeSheet->getCell('C'.$r)->getValue()));
+                $projects[] = str_replace(' ', '', $activeSheet->getCell('A'.$r)->getValue()).
+                    date('ymd', PHPExcel_Shared_Date::ExcelToPHP($activeSheet->getCell('B'.$r)->getValue()));
             }
         }
         $projects = array_unique($projects);
         if (count($projects) > 0) foreach ($activeSheet->getRowIterator() as $row) {
             if (($r = $row->getRowIndex()) == 1) continue;
-            if (in_array(str_replace(' ', '', $activeSheet->getCell('B'.$r)->getValue()).
-                    date('ymd', PHPExcel_Shared_Date::ExcelToPHP($activeSheet->getCell('C'.$r)->getValue())), $projects)) {
-                $key = str_replace(' ', '', $activeSheet->getCell('B'.$r)->getValue()).
-                    date('ymd', PHPExcel_Shared_Date::ExcelToPHP($activeSheet->getCell('C'.$r)->getValue()));
+            if (in_array(str_replace(' ', '', $activeSheet->getCell('A'.$r)->getValue()).
+                    date('ymd', PHPExcel_Shared_Date::ExcelToPHP($activeSheet->getCell('B'.$r)->getValue())), $projects)) {
+                $key = str_replace(' ', '', $activeSheet->getCell('A'.$r)->getValue()).
+                    date('ymd', PHPExcel_Shared_Date::ExcelToPHP($activeSheet->getCell('B'.$r)->getValue()));
                 if (!array_key_exists($key, $data))
                     $data[$key] = array(
-                        'project'   => $activeSheet->getCell('B'.$r)->getValue(),
-                        'etd'       => date('md', PHPExcel_Shared_Date::ExcelToPHP($activeSheet->getCell('C'.$r)->getValue())),
+                        'project'   => $activeSheet->getCell('A'.$r)->getValue(),
+                        'etd'       => date('md', PHPExcel_Shared_Date::ExcelToPHP($activeSheet->getCell('B'.$r)->getValue())),
                         'vessel_en' => $activeSheet->getCell('D'.$r)->getValue(),
                         'voyage'    => $activeSheet->getCell('E'.$r)->getValue(),
                         'vessel_cn' => $activeSheet->getCell('F'.$r)->getValue(),
